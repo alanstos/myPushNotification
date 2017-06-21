34d53388a7112cee12787d0e448f8443946df92f
Alan Araujo dos Santos <alan@MacBook-Pro-de-Alan.local>
2017-06-21 00:14:09 -0300

    inciio

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class  App extends Component {
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+      </View>
+    );
+  }
+}
